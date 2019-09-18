4a49210fac453094bf79c8b444107dddfc0f9140
houshihao <houshihao@vip.qq.com>
2019-09-18 15:25:41 +0800

    Create install-sss.sh

---
+++ install-sss.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Install Shadowsocks on CentOS 7
+
+echo "Installing Shadowsocks..."
+
+CONFIG_FILE=/etc/shadowsocks.json
+SERVICE_FILE=/etc/systemd/system/shadowsocks.service
+
+#-------------
+SS_PASSWORD=8V#r7k2}fBoC
+SS_PORT=28721
+#-------------
+
+SS_METHOD=aes-256-cfb
+SS_IP=`ip route get 1 | awk '{print $NF;exit}'`
+GET_PIP_FILE=/tmp/get-pip.py
+
+# install pip
+curl "https://bootstrap.pypa.io/get-pip.py" -o "${GET_PIP_FILE}"
+python ${GET_PIP_FILE}
+
+# install shadowsocks
+pip install --upgrade pip
+pip install shadowsocks
+
+# create shadowsocls config
+cat <<EOF | sudo tee ${CONFIG_FILE}
+{
+  "server": "0.0.0.0",
+  "server_port": ${SS_PORT},
+  "password": "${SS_PASSWORD}",
+  "method": "${SS_METHOD}"
+}
+EOF
+
+# create service
+cat <<EOF | sudo tee ${SERVICE_FILE}
+[Unit]
+Description=Shadowsocks
+
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/bin/ssserver -c ${CONFIG_FILE}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+systemctl enable shadowsocks
+systemctl start shadowsocks
+
+# view service status
+sleep 5
+systemctl status shadowsocks -l
+
+echo "================================"
+echo ""
+echo "Congratulations! Shadowsocks has been installed on your system."
+echo "You shadowsocks connection info:"
+echo "--------------------------------"
+echo "server:      ${SS_IP}"
+echo "server_port: ${SS_PORT}"
+echo "password:    ${SS_PASSWORD}"
+echo "method:      ${SS_METHOD}"
+echo "--------------------------------"
+
+echo "==========iptables  add ${SS_PORT} =================="
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${SS_PORT} -j ACCEPT
+
+iptables -nL --line-number |grep ${SS_PORT}
